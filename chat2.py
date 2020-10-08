115a3c7603c8650f4e02c44bf73ef7721c531d03
Masaakiii <71488961+Masaakiii@users.noreply.github.com>
2020-10-08 18:54:57 +0900

    frist commit

---
+++ chat2.py
@@ -0,0 +1,61 @@
+#讀取
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+
+#轉換
+def convert(lines):
+	person = None
+	allen_word_count = 0
+	viki_word_count = 0
+	allen_sticker_count = 0
+	viki_sticker_count = 0
+	allen_image_count = 0
+	viki_image_count = 0
+	for line in lines:
+		s = line.split(' ')
+		time = s[0]
+		name = s[1]
+		if name == 'Allen':
+			if s[2] == '貼圖':
+				allen_sticker_count += 1
+			elif s[2] == '圖片':
+				allen_image_count += 1
+			else:
+				for  m in s[2:]:
+					allen_word_count += len(m)
+
+		elif name == 'Viki':
+			if s[2] == '貼圖':
+				viki_sticker_count += 1
+			elif s[2] == '圖片':
+				viki_image_count += 1
+			else:
+				for m in s[2:]:
+					viki_word_count += len(m)
+	print('Allen說了', allen_word_count, '個文字')
+	print('Allen使用了', allen_sticker_count, '個貼圖')
+	print('Allen使用了', allen_image_count, '張圖片')
+	print('Viki說了',viki_word_count, '個文字')
+	print('Viki使用了', viki_sticker_count, '個貼圖')
+	print('Viki使用了', viki_image_count, '張圖片')
+
+
+#寫入
+def write_file(filename, lines):
+	with open(filename, 'w', encoding='utf-8-sig') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+
+#主要(進入點)
+def main():
+	lines = read_file('LINE-Viki.txt')
+	lines = convert(lines)
+	#write_file('output.txt', lines)
+
+main()
